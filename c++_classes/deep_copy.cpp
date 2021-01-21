590d1cac2c66b2206b281ddcef09d2f6204757fa
RitikaSingh02 <ritika2002singh@gmail.com>
2021-01-21 11:57:28 +0530

    DEEP COPY BY COPY CONSTRUCTOR

---
+++ c++_classes/deep_copy.cpp
@@ -0,0 +1,59 @@
+// https://www.javatpoint.com/cpp-copy-constructor
+
+#include <iostream>  
+using namespace std;  
+class Demo  
+{  
+    public:  
+    int a;  
+    int b;  
+    int *p;  
+  
+    Demo()  
+    {  
+        p=new int;  
+    }  
+    Demo(Demo &d)  
+    {  
+        a = d.a;  
+        b = d.b;  
+        p = new int;  
+        *p = *(d.p);  
+    }  
+    void setdata(int x,int y,int z)  
+    {  
+        a=x;  
+        b=y;  
+        *p=z;  
+    }  
+    void change(int n){
+        *p=n;
+    }
+    void showdata()  
+    {  
+        std::cout << "value of a is : " <<a<< std::endl;  
+        std::cout << "value of b is : " <<b<< std::endl;  
+        std::cout << "value of *p is : " <<*p<< std::endl;  
+    }  
+};  
+int main()  
+{  
+  Demo d1;  
+  d1.setdata(4,5,7);  
+  Demo d2 = d1;  
+  d2.change(5);
+
+  d2.showdata();  
+  d1.showdata();
+
+  //address of p is different fror both the obj
+
+//   value of a is : 4
+// value of b is : 5
+// value of *p is : 5
+// value of a is : 4
+// value of b is : 5
+// value of *p is : 7
+
+  return 0;  
+}  
