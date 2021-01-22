9901c408b9e566b63bcbf1f039e68fa425c6f7e1
RitikaSingh02 <ritika2002singh@gmail.com>
2021-01-22 17:33:34 +0530

    friend function in cpp

--- c++_classes/friend_funct.cpp
+++ c++_classes/friend_funct.cpp
@@ -26,4 +26,13 @@ int main()
     return 0;    
 } 
 
+// friend func is not the member func of a class to which it is friend
+// it must be defined outside the class to which it is a friend
+// they can access the members but not directly like the member func do
+// it isdeclared in the class with the keyword friend
+// it has no caller object
+// it should not be defined with the membership label
+// a same friend function can become friend of more than one class
+
+
 //friend func / class can access the private/protected members of the class
