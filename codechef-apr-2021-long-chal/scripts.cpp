4e7089895b932c19211aeb6030d0a1f9c1787149
RitikaSingh02 <ritika2002singh@gmail.com>
2021-04-09 19:49:16 +0530

    scripts and chef

--- codechef-apr-2021-long-chal/scripts.cpp
+++ codechef-apr-2021-long-chal/scripts.cpp
@@ -1,42 +1,42 @@
-https://www.codechef.com/APRIL21C/submit/SSCRIPT
+// https://www.codechef.com/APRIL21C/submit/SSCRIPT
 #include <bits/stdc++.h>
 #define ll long long;
 
 using namespace std;
 
-int main() {
+int main()
+{
     int n;
-    cin>>n;
-    while(n--)
+    cin >> n;
+    while (n--)
     {
-        int  l , k;
-        cin>> l >> k;
+        int l, k;
+        cin >> l >> k;
         string s;
-        cin>>s;
+        cin >> s;
         bool t = false;
-        int counter =0;
-        for( int i =0 ;i<l ; i++ )
+        int counter = 0;
+        for (int i = 0; i < l; i++)
         {
-            if(s[i]=='*')
+            if (s[i] == '*')
             {
-                
-                    counter++;
-                    if(counter>=k )
-                    {   
-                        t = true;
-                        break;
-                    }
-                
-            }    
+
+                counter++;
+                if (counter >= k)
+                {
+                    t = true;
+                    break;
+                }
+            }
             else
-                counter=0;
+                counter = 0;
         }
-        
-        if(t==false)
-            cout<<"NO\n";
+
+        if (t == false)
+            cout << "NO\n";
         else
-            cout<<"YES\n";
+            cout << "YES\n";
     }
-	// your code goes here
-	return 0;
+    // your code goes here
+    return 0;
 }
