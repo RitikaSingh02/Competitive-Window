5a03b27c4d4d86ae37f068e3a924f3551c5ae313
RITIKA SINGH <ritika2002singh@gmail.com>
2021-08-10 00:22:42 +0530

    O(n2) , suffix partitioning approach

--- leetcoding-Aug-2021/pallindrom-partitioning-ii.cpp
+++ leetcoding-Aug-2021/pallindrom-partitioning-ii.cpp
@@ -1,4 +1,4 @@
-//https://leetcode.com/problems/palindrome-partitioning-ii/
+// https://leetcode.com/problems/palindrome-partitioning-ii/
 class Solution {
 public:
     int minCut(string s) {
@@ -31,50 +31,31 @@ public:
                }
            }
         }
-        //constructing a 2d array which will contain the no of cuts
-        int dp1[n][n];
-        for(int g = 0 ; g < n ; g++)
+        //suffix partitioning concept
+        int dp1[n] ;
+        dp1[0]=0;
+        // for a string say "abccbccca"
+        //j=1 means result of a
+        //j = 3 means result of abc and so on so aur ans is in j = n
+        for(int j = 1 ; j < n ; j++)
         {
-            for(int i = 0 , j = g ; j < n ; j++, i++)
+            if(dp[0][j])
+                dp1[j] = 0;
+            else
             {
-                if(g==0)
+                int min_cut = INT_MAX;
+                for(int i = j ; i>=1 ; i --)
                 {
-                    dp1[i][j]=0;
-                }
-                else
-                {
-                    if(g==1)                   
+                    if(dp[i][j])
                     {
-                        if(s[i] == s[j])
-                            dp1[i][j]=0;
-                        else
-                            dp1[i][j]=1;
-                    }
-                    else
-                    {
-                        if(dp[i][j])
-                        {
-                            dp1[i][j]=0;
-                        }
-                        else
-                        {
-                            int min_cut = INT_MAX;
-                            for(int cut_index = 0 ; cut_index < j ; cut_index++)
-                            {
-                                int first_part = dp1[i][cut_index];
-                                int second_part = dp1[cut_index+1][j];
-                                if(first_part<min_cut && second_part<min_cut)
-                                {
-                                    min_cut = min(first_part + second_part +1 , min_cut);
-                                }
-                                    
-                            }
-                            dp1[i][j] = min_cut;
-                        }
+                        // if the suffix is pallindrome then check for the left over prefix
+                        min_cut = min(min_cut , dp1[i-1]);
                     }
                 }
+                dp1[j] = min_cut +1;
             }
         }
-        return dp1[0][n-1];
+        
+        return dp1[n-1];
     }
 };
