2015d8d35604963de10a2dac727ba602f75b4981
RITIKA SINGH <ritika2002singh@gmail.com>
2021-08-09 20:55:43 +0530

    Create n-ary-tree-preorder-traversal.cpp

---
+++ leetcoding-Aug-2021/n-ary-tree-preorder-traversal.cpp
@@ -0,0 +1,34 @@
+//https://leetcode.com/problems/n-ary-tree-preorder-traversal
+class Solution {
+public:
+    vector<vector<int>>res;
+    vector<vector<int>> levelOrder(Node* root) {
+        queue<Node*>q;
+        if(root)
+        {
+            q.push(root);
+            }
+        else
+            return res;
+        while(!q.empty())
+        {
+            vector<int>temp;
+            int n = q.size();
+            for(int i = 0 ; i <n ; i++)            
+            {
+                Node *t = q.front();
+                q.pop();
+                temp.push_back(t->val);
+                for(auto j : t->children)
+                {
+                    q.push(j);
+                }
+
+            }
+                res.push_back(temp);
+                temp.clear();
+            
+        }
+        return res;
+    }
+};
