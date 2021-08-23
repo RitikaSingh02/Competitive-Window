3ce084432f3117d59c3a9d2777ced6cc5421bb63
RITIKA SINGH <ritika2002singh@gmail.com>
2021-08-23 17:07:21 +0530

    Update pascal's_triangle.cpp

--- SDE-SHEET-STIVER/pascal's_triangle.cpp
+++ SDE-SHEET-STIVER/pascal's_triangle.cpp
@@ -1,56 +1,20 @@
+//iterative approach
 class Solution {
 public:
-    vector<int>last;
-    vector<vector<int>>res;
-    void construct(vector<int>&temp , vector<int>samp)
-    {
-        int size = temp.size();
-        for(int i = 1  , j =0 ; i <=size-2 ;i++ , j++)
-        {
-            temp[i] = samp[j];
-        }
-    }
-    void fun(int s , int n)    
-    {
-      last = res[res.size()-1];//{1,2,1}
-        // for(auto i : last)
-        //     cout<<i;
-        // cout<<"\n";
-        if(last.size() == n )
-            return;
-        vector<int>temp(last.size() +1 , 1);
-        vector<int>samp;
-        for(int i =0 ; i < s-1 ; i++)
-        {
-            samp.push_back(last[i] +last[i+1]);
-        }
+    vector<int> getRow(int rowIndex) {
         
-        construct(temp , samp);
-        res.push_back(temp);
-        // cout<<res.size();
-        
-        fun(temp.size() , n);
-    }
-    vector<vector<int>> generate(int numRows) {
-     //recursive
-        int n = numRows;
-        if(n>=3)
+        //iterative
+        vector<vector<int>>res(rowIndex +1);
+        for(int i = 0 ; i <=rowIndex ; i++)
         {
-            res.push_back({1});
-            res.push_back({1,1});
-            res.push_back({1,2,1});
-        }
-        else
-            if(n==2)
+            res[i].resize(i+1);
+            res[i][0] = res[i][i] = 1;
+            //res[i][i] coz i+1 ki array hai to last ele hoga i aur wo 1 hi hona hai!
+            for(int j = 1 ; j<i ; j++)
             {
-                res.push_back({1});
-                res.push_back({1,1});
-                
+                res[i][j] = res[i-1][j-1]+res[i-1][j];
             }
-            else
-                if(n==1)
-                    res.push_back({1});
-        fun(3, n );
-        return res;
+        }
+        return res[rowIndex];
     }
 };
