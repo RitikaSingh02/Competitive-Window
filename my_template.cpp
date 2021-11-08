2c01f519d30f9e72f24c13b534714daaff28c6d2
RITIKA SINGH <ritika2002singh@gmail.com>
2021-11-08 19:48:37 +0530

    Create my_template.cpp

---
+++ my_template.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fo(i,n) for(i=0;i<n;i++)
+#define ll long long
+#define riz(x) cout << #x << "=" << x << endl
+#define riz1(x) cout << x<<" ";
+#define all(x) x.begin(), x.end()
+#define mem(x) memset(x, 0, sizeof(x))
+#define sortall(x) sort(all(x))
+#define tr(it, a) for(auto it = a.begin(); it != a.end(); it++) riz1(*it);
+#define PI 3.1415926535897932384626
+typedef vector<int> vi;
+typedef queue<int> qi;
+typedef set<int> si;
+
+int main()
+{
+    return 0;
+}
