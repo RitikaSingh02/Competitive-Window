a3160a9f068a89ee2d08fe9f0256239a236e720d
RITIKA SINGH <ritika2002singh@gmail.com>
2022-03-02 01:09:21 +0530

    Update triangle.cpp

--- dynamic_programming/2-D Dp - Variable ending point/triangle.cpp
+++ dynamic_programming/2-D Dp - Variable ending point/triangle.cpp
@@ -36,3 +36,20 @@ int minimumPathSum(vector<vector<int>>& triangle, int n){
 }
 
 // tabulation
+int minimumPathSum(vector<vector<int>>& triangle, int n){
+    vector<vector<int>>dp(n , vector<int>(n , -1));
+    for(int i = 0 ; i < n ; i++) dp[n-1][i] = triangle[n-1][i];
+    
+    for(int row = n - 2; row >= 0 ; row--)
+    {
+        for(int col = row ; col >= 0 ; col--)
+        {
+            int down = triangle[row][col] + dp[row + 1 ][ col ];
+    		int diag = triangle[row][col] + dp[row + 1][col + 1];
+            dp[row][col] = min(down , diag);
+        }
+    }
+   
+    return dp[0][0];
+    
+}
